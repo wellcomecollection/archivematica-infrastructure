7cf412ca212ad69c8d3f40f2bdb3515a19e9011e
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-11 17:26:32 +0000

    Okay, and now sort out the client

--- dockerfiles/install_mcp_client.sh
+++ dockerfiles/install_mcp_client.sh
@@ -10,5 +10,10 @@ apt-get update
 # for mail configuration.
 DEBIAN_FRONTEND=noninteractive apt-get install --yes archivematica-mcp-client
 
+# If you don't install the dashboard, the server complains about not being
+# able to find the 'components' module.
+apt-get install --yes libmysqlclient-dev
+DEBIAN_FRONTEND=noninteractive apt-get install --yes archivematica-dashboard
+
 apt autoremove --yes
 apt-get clean
