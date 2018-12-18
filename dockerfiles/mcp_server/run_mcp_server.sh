32cad1ac5e4da09be2cadf0a3c861d821c7b6d30
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-18 12:00:02 +0000

    Enough to get the initial set of containers up and running

--- dockerfiles/mcp_server/run_mcp_server.sh
+++ dockerfiles/mcp_server/run_mcp_server.sh
@@ -6,16 +6,24 @@ set -o verbose
 
 # This is a proxy for whether the storage service has started yet (and applied
 # the database migrations that the MCP server requires).
-for i in 1 2 3 4 5
-do
-  if curl "http://dashboard:8000/"
-  then
-    echo "Dashboard is up!  Starting"
-    break
-  else
-    echo "Dashboard not up yet; sleeping"
-    sleep 10
-  fi
-done
+sleep 20
+# for i in 1 2 3 4 5 6 7 8 9 10
+# do
+#   if curl "http://localhost:9000/"
+#   then
+#     echo "Dashboard is up!  Starting"
+#     break
+#   else
+#     echo "Dashboard not up yet; sleeping"
+#     sleep 20
+#   fi
+# done
+#
+# if curl "http://localhost:9000/"
+# then
+#   echo "Dashboard has not started successfully!"
+#   exit 1
+# fi
+
 
 /usr/share/archivematica/virtualenvs/archivematica-mcp-server/bin/python /usr/lib/archivematica/MCPServer/archivematicaMCP.py
