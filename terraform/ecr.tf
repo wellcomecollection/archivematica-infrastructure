47ffc65997e4f7f6a06b48da0ecf9a832ac85ee6
Alex Chan <a.chan@wellcome.ac.uk>
2018-12-19 10:51:56 +0000

    Publish custom nginx containers to ECR

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -13,6 +13,11 @@ module "ecr_mcp_server" {
   name   = "archivematica_mcp_server"
 }
 
+module "ecr_nginx" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "archivematica_nginx"
+}
+
 module "ecr_storage_service" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "archivematica_storage_service"
