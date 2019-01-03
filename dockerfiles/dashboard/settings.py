8736236373e586d0369142ffbc6392d66629f7d2
Alex Chan <a.chan@wellcome.ac.uk>
2019-01-03 11:31:42 +0000

    Get /archivematica/dashboard to redirect to a login page
    Before this patch, trying to access the dashboard when not logged in
    went as follows:
    
    GET https://workflow.wc.org/archivematica/dashboard/
    302 FOUND > workflow.wc.org/administration/accounts/login/
    
    which 404s, because it's not an Archivematica specific path.
    
    It now has the slightly more correct behaviour:
    
    GET https://workflow.wc.org/archivematica/dashboard/
    302 FOUND > workflow.wc.org/archivematica/dashboard/administration/accounts/login/
    
    which returns the login page.
    
    We do this by passing the SCRIPT_NAME env var to WSGI, which tells it
    to prefix most URLs with /archivematica/dashboard (and silently removes
    it from Django for us) -- it's just the hard-coded login in
    middleware.py that causes us some annoyance.

--- dockerfiles/dashboard/settings.py
+++ dockerfiles/dashboard/settings.py
@@ -1,6 +1,19 @@
+# -*- encoding: utf-8
+
 from __future__ import absolute_import
 
 from .production import *
 
-STATIC_URL = "/archivematica/dashboard/"
-FORCE_SCRIPT_NAME = "/archivematica/dashboard"
+
+# This setting is a workaround for some slightly dodgy behaviour in Archivematica.
+# Specifically, when a user tries to hit a page while not logged in, they get
+# redirected to this hard-coded login URL.  This bypasses any other URL prefixes
+# or rewriting, in particular it doesn't work when served behind a path
+# (/archivematica/dashboard/).
+#
+# https://github.com/artefactual/archivematica/blob/174736794b96a504f782978bca935d5697f03703/src/dashboard/src/installer/middleware.py#L39-L43
+#
+# You can see the settings being overridden here:
+# https://github.com/artefactual/archivematica/blob/2ffe3e8e5485dc038a3fb4fafe3723a1e4a45974/src/dashboard/src/settings/base.py#L397
+
+LOGIN_URL = "/archivematica/dashboard/administration/accounts/login/"
