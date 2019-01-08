4cdfe1ab7b1f8c5457a35a3ed16ca76687255dd4
Alex Chan <a.chan@wellcome.ac.uk>
2019-01-08 14:59:55 +0000

    Create a separate nginx image for the dashboard

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -3,6 +3,11 @@ module "ecr_dashboard" {
   name   = "archivematica_dashboard"
 }
 
+module "ecr_dashboard_nginx" {
+  source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
+  name   = "archivematica_dashboard_nginx"
+}
+
 module "ecr_mcp_client" {
   source = "git::https://github.com/wellcometrust/terraform.git//ecr?ref=v1.0.0"
   name   = "archivematica_mcp_client"
