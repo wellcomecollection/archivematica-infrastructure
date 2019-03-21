69ef9acae23805e6a756d5b69eb0fcc943c6f9a3
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-03-21 12:02:01 +0000

    Set up task role permissions for storage service

--- terraform/services.tf
+++ terraform/services.tf
@@ -139,6 +139,25 @@ resource "random_integer" "storage_service_django_key" {
   }
 }
 
+resource "aws_iam_role_policy" "storage_service_task_role_policy" {
+  role   = "${module.storage_service.task_role_name}"
+  policy = "${data.aws_iam_policy_document.storage_service_aws_permissions.json}"
+}
+
+
+data "aws_iam_policy_document" "storage_service_aws_permissions" {
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::wellcomecollection-storage-ingests/",
+    ]
+  }
+}
+
+
 module "storage_service" {
   source = "./nginx_service"
 
