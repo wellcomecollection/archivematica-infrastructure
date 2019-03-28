70a62cedb4471600716fe6078999209e8a1668ba
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-28 11:42:06 +0000

    Create a dedicated bucket for archivematica ingests

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,39 +1,3 @@
-locals {
-  ingests_drop_bucket_name = "wellcomecollection-storage-ingests"
-}
-
-# Give permission for the storage service microservice to upload objects to
-# the ingests drop bucket.
-
-resource "aws_s3_bucket_policy" "ingests_put" {
-  bucket = "${local.ingests_drop_bucket_name}"
-  policy = "${data.aws_iam_policy_document.ingests_put.json}"
-
-  provider = "aws.storage"
-}
-
-data "aws_iam_policy_document" "ingests_put" {
-  statement {
-    actions = [
-      "s3:Put*",
-      "s3:Get*",
-    ]
-
-     resources = [
-       "arn:aws:s3:::${local.ingests_drop_bucket_name}",
-       "arn:aws:s3:::${local.ingests_drop_bucket_name}/*",
-    ]
-
-     principals {
-      type = "AWS"
-
-       identifiers = [
-        "${module.storage_service.task_role_arn}",
-      ]
-    }
-  }
-}
-
 resource "aws_iam_role_policy" "storage_service_task_role_policy" {
   role   = "${module.storage_service.task_role_name}"
   policy = "${data.aws_iam_policy_document.storage_service_aws_permissions.json}"
@@ -46,9 +10,8 @@ data "aws_iam_policy_document" "storage_service_aws_permissions" {
       "s3:Put*",
     ]
 
-    # TODO: Scope this more tightly to a location specifically for Archivematica.
     resources = [
-      "arn:aws:s3:::${local.ingests_drop_bucket_name}/*",
+      "${aws_s3_bucket.archivematica_drop.arn}/*",
     ]
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,16 +3,6 @@ provider "aws" {
   version = "1.54.0"
 
   assume_role {
-    role_arn = "arn:aws:iam::299497370133:role/developer"
-  }
-}
-
-provider "aws" {
-  alias   = "storage"
-  region  = "${var.region}"
-  version = "1.54.0"
-
-  assume_role {
-    role_arn = "arn:aws:iam::975596993436:role/admin"
+    role_arn = "arn:aws:iam::299497370133:role/admin"
   }
 }
