b5810084492564e52c039b8d09484c1c1246b3b9
Alex Chan <a.chan@wellcome.ac.uk>
2019-11-11 14:03:43 +0000

    Bump the AWS provider to a newer version
    This is prep for upgrading to terraform 0.12.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region  = "${var.region}"
-  version = "1.54.0"
+  version = "2.35.0"
 
   assume_role {
-    role_arn = "arn:aws:iam::299497370133:role/workflow-admin"
+    role_arn = "arn:aws:iam::299497370133:role/workflow-developer"
   }
 }
