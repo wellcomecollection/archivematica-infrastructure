d6cc2b8e567aa433f48e66ea97753bc9137e1392
Alex Chan <alex@alexwlchan.net>
2019-11-12 13:47:42 +0000

    don't have two drop buckets

--- terraform/modules/critical/iam.tf
+++ terraform/modules/critical/iam.tf
@@ -1,16 +1,16 @@
-resource "aws_s3_bucket_policy" "allow_storage_service_unpacker_drop_read" {
-  bucket = aws_s3_bucket.archivematica_drop.id
-  policy = data.aws_iam_policy_document.allow_drop_bucket_read.json
+resource "aws_s3_bucket_policy" "allow_storage_service_unpacker_ingests_read" {
+  bucket = aws_s3_bucket.archivematica_ingests.id
+  policy = data.aws_iam_policy_document.allow_ingests_bucket_read.json
 }
 
-data "aws_iam_policy_document" "allow_drop_bucket_read" {
+data "aws_iam_policy_document" "allow_ingests_bucket_read" {
   statement {
     actions = [
       "s3:Get*",
     ]
 
     resources = [
-      "${aws_s3_bucket.archivematica_drop.arn}/*",
+      "${aws_s3_bucket.archivematica_ingests.arn}/*",
     ]
 
     principals {
--- terraform/modules/critical/s3_ingests.tf
+++ terraform/modules/critical/s3_ingests.tf
@@ -2,3 +2,4 @@ resource "aws_s3_bucket" "archivematica_ingests" {
   bucket = "wellcomecollection-archivematica-${var.namespace}-ingests"
   acl    = "private"
 }
+
