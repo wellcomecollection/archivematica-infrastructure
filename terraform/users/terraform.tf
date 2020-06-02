c25a46f9af699c361d8b05b407484c5f371fcca0
Alex Chan <alex@alexwlchan.net>
2020-06-02 08:45:47 +0100

    Make sure we actually use the prod config for prod
    Previously this meant the same bucket name was being copied into the
    permissions block, so archivists could only upload to staging.

--- terraform/users/terraform.tf
+++ terraform/users/terraform.tf
@@ -29,7 +29,7 @@ data "terraform_remote_state" "critical_prod" {
     role_arn = "arn:aws:iam::299497370133:role/workflow-read_only"
 
     bucket = "wellcomecollection-workflow-infra"
-    key    = "terraform/archivematica-infra/critical_staging.tfstate"
+    key    = "terraform/archivematica-infra/critical_prod.tfstate"
     region = "eu-west-1"
   }
 }
