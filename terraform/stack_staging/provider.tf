c66c0e6bc1f4347b8f73788c897abd3b0b8f2572
Alex Chan <alex@alexwlchan.net>
2020-07-06 14:38:28 +0100

    Use the new DNS role in the application stacks
    Also move the providers to the top level.  This lets us specify the
    version pins in one place, rather than having them distributed
    throughout the configuration.

--- terraform/stack_staging/provider.tf
+++ terraform/stack_staging/provider.tf
@@ -1,8 +1,28 @@
 provider "aws" {
   region  = var.region
-  version = "2.35.0"
+  version = "~> 2.69"
 
   assume_role {
     role_arn = "arn:aws:iam::299497370133:role/workflow-admin"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.69"
+  alias   = "digitisation"
+
+  assume_role {
+    role_arn = "arn:aws:iam::404315009621:role/digitisation-admin"
+  }
+}
+
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.69"
+  alias   = "dns"
+
+  assume_role {
+    role_arn = "arn:aws:iam::267269328833:role/wellcomecollection-assume_role_hosted_zone_update"
   }
 }
