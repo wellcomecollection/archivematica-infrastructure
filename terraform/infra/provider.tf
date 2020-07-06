1aab6cc208a714d8c3150d4d3ab787a993400d67
Alex Chan <alex@alexwlchan.net>
2020-07-06 14:42:24 +0100

    Use the new DNS role for the infra

--- terraform/infra/provider.tf
+++ terraform/infra/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = var.region
-  version = "2.35.0"
+  version = "~> 2.69"
 
   assume_role {
     role_arn = "arn:aws:iam::299497370133:role/workflow-developer"
@@ -9,10 +9,10 @@ provider "aws" {
 
 provider "aws" {
   region  = "eu-west-1"
-  version = "2.35.0"
-  alias   = "routermaster"
+  version = "~> 2.69"
+  alias   = "dns"
 
   assume_role {
-    role_arn = "arn:aws:iam::250790015188:role/wellcomecollection-assume_role_hosted_zone_update"
+    role_arn = "arn:aws:iam::267269328833:role/wellcomecollection-assume_role_hosted_zone_update"
   }
 }
