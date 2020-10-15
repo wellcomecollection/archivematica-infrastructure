96b2f5b37eb81528b053aa2cd34644e095947fcf
Alex Chan <alex@alexwlchan.net>
2020-10-15 12:59:51 +0100

    Give the archivists permission to read from the storage service

--- terraform/users/iam_archivists.tf
+++ terraform/users/iam_archivists.tf
@@ -38,3 +38,34 @@ resource "aws_iam_user_policy" "allow_archivists_s3_upload" {
   user   = aws_iam_user.archivists_s3_upload.name
   policy = data.aws_iam_policy_document.allow_s3_upload.json
 }
+
+data "aws_iam_policy_document" "allow_s3_download" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:List*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::wellcomecollection-storage",
+      "arn:aws:s3:::wellcomecollection-storage/*",
+      "arn:aws:s3:::wellcomecollection-storage-staging",
+      "arn:aws:s3:::wellcomecollection-storage-staging/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:List*",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_user_policy" "allow_archivists_s3_download" {
+  user   = aws_iam_user.archivists_s3_upload.name
+  policy = data.aws_iam_policy_document.allow_s3_download.json
+}
