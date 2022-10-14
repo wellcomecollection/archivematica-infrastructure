01fd8c8ef5f542c087ab8e7d02cdf1b333bc082e
Alex Chan <a.chan@wellcome.org>
2022-10-14 15:25:43 +0100

    start using the new images in staging

--- terraform/stack_staging/locals.tf
+++ terraform/stack_staging/locals.tf
@@ -3,5 +3,10 @@ locals {
 
   lambda_error_alarm_arn = local.monitoring_outputs["workflow_lambda_error_alerts_topic_arn"]
 
-  ecr_storage_service_repo_url = data.terraform_remote_state.infra.outputs.ecr_storage_service_repo_url
+  infra_state = data.terraform_remote_state.infra.outputs
+
+  ecr_dashboard_repo_url       = local.infra_state["ecr_dashboard_repo_url"]
+  ecr_mcp_client_repo_url      = local.infra_state["ecr_mcp_client_repo_url"]
+  ecr_mcp_server_repo_url      = local.infra_state["ecr_mcp_server_repo_url"]
+  ecr_storage_service_repo_url = local.infra_state["ecr_storage_service_repo_url"]
 }
