736663d7f4a5b169f4c601ea8bffb2c48488c014
Alex Chan <a.chan@wellcome.org>
2022-10-14 12:41:12 +0100

    better overlay naming

---
+++ dockerfiles/archivematica/README.md
@@ -0,0 +1,11 @@
+# archivematica
+
+This folder creates the following images:
+
+*   dashboard
+*   MCPClient
+*   MCPServer
+
+We only diverge slightly from Archivematica upstream, so rather than maintaining a whole separate fork, we have a series of overlays.
+
+The `vendor` directory is a shallow copy of the Artefactual repo, with just the files
--- dockerfiles/archivematica/copy_vendored_files.py
+++ dockerfiles/archivematica/copy_vendored_files.py
@@ -20,32 +20,32 @@ if __name__ == '__main__':
     # Creates a list of (name) -> (file pair)
     #
     # e.g. src/archivematicaCommon/files.py -> {
-    #        old -> /path/to/repo/src/archivematicaCommon/files.old.py
-    #        new -> /path/to/repo/src/archivematicaCommon/files.new.py
+    #        artefactual -> /path/to/repo/src/archivematicaCommon/files.artefactual.py
+    #        wellcome -> /path/to/repo/src/archivematicaCommon/files.wellcome.py
     #      }
-    file_pairs = collections.defaultdict(lambda: {'old': None, 'new': None})
+    file_pairs = collections.defaultdict(lambda: {'artefactual': None, 'wellcome': None})
 
     for fp in get_file_paths_under(vendor_dir):
-        base_path = fp.replace('.old', '').replace('.new', '')
+        base_path = fp.replace('.artefactual', '').replace('.wellcome', '')
 
-        if '.old' in fp:
-            file_pairs[os.path.relpath(base_path, vendor_dir)]['old'] = fp
+        if '.artefactual' in fp:
+            file_pairs[os.path.relpath(base_path, vendor_dir)]['artefactual'] = fp
 
-        if '.new' in fp:
-            file_pairs[os.path.relpath(base_path, vendor_dir)]['new'] = fp
+        if '.wellcome' in fp:
+            file_pairs[os.path.relpath(base_path, vendor_dir)]['wellcome'] = fp
 
     # Now go through the file pairs.
     #
-    # If the old file doesn't match what's in Artefactual upstream, we need
+    # If the artefactual file doesn't match what's in Artefactual upstream, we need
     # to recheck our changes -- do they still apply?
     #
-    # Otherwise we copy the old file over the new file.
+    # Otherwise we copy the artefactual file over the wellcome file.
     for name, pair in file_pairs.items():
         if None in pair.values():
             raise ValueError(f'Did not get a pair of vendored files for {name}')
 
-        if not filecmp.cmp(pair['old'], name):
-            raise ValueError(f'Old file for {name} doesn’t match upstream!')
+        if not filecmp.cmp(pair['artefactual'], name):
+            raise ValueError(f'artefactual file for {name} doesn’t match upstream!')
 
         print(f'Copying vendored file {name}')
-        shutil.copyfile(pair['new'], name)
+        shutil.copyfile(pair['wellcome'], name)
