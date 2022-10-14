a46eba84467d3783fbdd123241e62ced1c645fff
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-10-14 14:09:00 +0000

    Apply auto-formatting rules

--- dockerfiles/archivematica/copy_overlay_files.py
+++ dockerfiles/archivematica/copy_overlay_files.py
@@ -13,7 +13,7 @@ def get_file_paths_under(root):
             yield os.path.join(dirpath, f)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     overlay_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "overlay")
 
     # Creates a list of (name) -> (file pair)
@@ -22,16 +22,18 @@ if __name__ == '__main__':
     #        artefactual -> /path/to/repo/src/archivematicaCommon/files.artefactual.py
     #        wellcome -> /path/to/repo/src/archivematicaCommon/files.wellcome.py
     #      }
-    file_pairs = collections.defaultdict(lambda: {'artefactual': None, 'wellcome': None})
+    file_pairs = collections.defaultdict(
+        lambda: {"artefactual": None, "wellcome": None}
+    )
 
     for fp in get_file_paths_under(overlay_dir):
-        base_path = fp.replace('.artefactual', '').replace('.wellcome', '')
+        base_path = fp.replace(".artefactual", "").replace(".wellcome", "")
 
-        if '.artefactual' in fp:
-            file_pairs[os.path.relpath(base_path, overlay_dir)]['artefactual'] = fp
+        if ".artefactual" in fp:
+            file_pairs[os.path.relpath(base_path, overlay_dir)]["artefactual"] = fp
 
-        if '.wellcome' in fp:
-            file_pairs[os.path.relpath(base_path, overlay_dir)]['wellcome'] = fp
+        if ".wellcome" in fp:
+            file_pairs[os.path.relpath(base_path, overlay_dir)]["wellcome"] = fp
 
     # Now go through the file pairs.
     #
@@ -41,10 +43,10 @@ if __name__ == '__main__':
     # Otherwise we copy the artefactual file over the wellcome file.
     for name, pair in file_pairs.items():
         if None in pair.values():
-            raise ValueError(f'Did not get a pair of overlayed files for {name}')
+            raise ValueError(f"Did not get a pair of overlayed files for {name}")
 
-        if not filecmp.cmp(pair['artefactual'], name):
-            raise ValueError(f'artefactual file for {name} doesn’t match upstream!')
+        if not filecmp.cmp(pair["artefactual"], name):
+            raise ValueError(f"artefactual file for {name} doesn’t match upstream!")
 
-        print(f'Copying overlayed file {name}')
-        shutil.copyfile(pair['wellcome'], name)
+        print(f"Copying overlayed file {name}")
+        shutil.copyfile(pair["wellcome"], name)
