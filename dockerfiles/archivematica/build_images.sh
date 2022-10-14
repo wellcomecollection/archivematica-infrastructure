2a30e6cc31bb4df8ac8098fa0814a8b639b8ee9f
Alex Chan <a.chan@wellcome.org>
2022-10-14 12:51:49 +0100

    Actually build the Docker images

--- dockerfiles/archivematica/build_images.sh
+++ dockerfiles/archivematica/build_images.sh
@@ -19,4 +19,13 @@ pushd $(mktemp -d)
   echo "*** Applying overlay files to repository"
   python3 "$ROOT/dockerfiles/archivematica/copy_overlay_files.py"
   git status
+
+  echo "*** Building the dashboard"
+  cd hack
+
+  for service in mcp-server mcp-client dashboard
+  do
+    docker-compose build "archivematica-$service"
+    docker tag "archivematica-$service" "archivematica-$service:$ARCHIVEMATICA_TAG"
+  done
 popd
