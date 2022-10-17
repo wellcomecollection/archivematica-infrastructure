06f74eac648df8b0e8f35fbaa5f94ad1d4a374f6
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-10-17 12:43:50 +0000

    Apply auto-formatting rules

--- terraform/stack_staging/locals.tf
+++ terraform/stack_staging/locals.tf
@@ -6,12 +6,12 @@ locals {
   infra_state = data.terraform_remote_state.infra.outputs
 
   ecr_repo_urls = {
-    dashboard = local.infra_state["ecr_dashboard_repo_url"]
-    mcp_client      = local.infra_state["ecr_mcp_client_repo_url"]
-    mcp_server      = local.infra_state["ecr_mcp_server_repo_url"]
+    dashboard          = local.infra_state["ecr_dashboard_repo_url"]
+    mcp_client         = local.infra_state["ecr_mcp_client_repo_url"]
+    mcp_server         = local.infra_state["ecr_mcp_server_repo_url"]
     am_storage_service = local.infra_state["ecr_storage_service_repo_url"]
-    clamavd         = local.infra_state["ecr_clamavd_repo_url"]
-    nginx           = local.infra_state["ecr_nginx_repo_url"]
+    clamavd            = local.infra_state["ecr_clamavd_repo_url"]
+    nginx              = local.infra_state["ecr_nginx_repo_url"]
   }
 
   ecr_image_tags = {
