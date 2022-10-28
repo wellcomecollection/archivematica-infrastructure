d006aeed9db299be6241fb3316450de8d790f486
Alex Chan <a.chan@wellcome.org>
2022-10-28 10:59:41 +0100

    have yet another go at tagging

--- lambdas/s3_start_transfer/src/s3_start_transfer.py
+++ lambdas/s3_start_transfer/src/s3_start_transfer.py
@@ -36,11 +36,9 @@ def _write_log(sess, logger, bucket, key, result, tags=None):
         Bucket=bucket,
         Key=log_key,
         Body=logger.text(),
-        Tagging={"TagSet":[
-            {"Key": key, "Value": value}
-            for key, value in tags.items()
-            if value is not None
-        ]},
+        Tagging=" ".join(
+            f"{key}={value}" for key, value in tags.items() if value is not None
+        ),
         # The object is uploaded by a Lambda running in the workflow account,
         # but the transfer bucket is owned by the digitisation bucket.
         #
