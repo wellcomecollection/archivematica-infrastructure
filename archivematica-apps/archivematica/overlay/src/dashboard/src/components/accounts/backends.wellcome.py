a4b0c3631097527a20d5abeaef7e863178d9ca14
Alex Chan <a.chan@wellcome.org>
2022-10-31 11:40:01 +0000

    log the ID info

--- archivematica-apps/archivematica/overlay/src/dashboard/src/components/accounts/backends.wellcome.py
+++ archivematica-apps/archivematica/overlay/src/dashboard/src/components/accounts/backends.wellcome.py
@@ -59,6 +59,8 @@ class CustomOIDCBackend(OIDCAuthenticationBackend):
             assert user_attr not in info
             info[user_attr] = access_info[oidc_attr]
 
+        print(id_info)
+
         for oidc_attr, user_attr in settings.OIDC_ID_ATTRIBUTE_MAP.items():
             assert user_attr not in info
             info[user_attr] = id_info[oidc_attr]
