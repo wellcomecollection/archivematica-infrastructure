ee547d6e401fb4e894a65b0fd6baebcff8990847
Alex Chan <alex@alexwlchan.net>
2023-02-08 13:07:20 +0000

    try fixing how the email address is defined

--- archivematica-apps/archivematica/overlay/src/dashboard/src/components/accounts/backends.wellcome.py
+++ archivematica-apps/archivematica/overlay/src/dashboard/src/components/accounts/backends.wellcome.py
@@ -59,13 +59,15 @@ class CustomOIDCBackend(OIDCAuthenticationBackend):
         pprint(id_info)
         pprint(access_info)
 
-        for oidc_attr, user_attr in settings.OIDC_ACCESS_ATTRIBUTE_MAP.items():
-            assert user_attr not in info
-            info[user_attr] = access_info[oidc_attr]
+        info['email'] = id_info['upn']
 
-        for oidc_attr, user_attr in settings.OIDC_ID_ATTRIBUTE_MAP.items():
-            assert user_attr not in info
-            info[user_attr] = id_info[oidc_attr]
+        # for oidc_attr, user_attr in settings.OIDC_ACCESS_ATTRIBUTE_MAP.items():
+        #     assert user_attr not in info
+        #     info[user_attr] = access_info[oidc_attr]
+        #
+        # for oidc_attr, user_attr in settings.OIDC_ID_ATTRIBUTE_MAP.items():
+        #     assert user_attr not in info
+        #     info[user_attr] = id_info[oidc_attr]
 
         return info
 
