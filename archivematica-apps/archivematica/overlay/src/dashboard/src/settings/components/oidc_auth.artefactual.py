1ca7b5db3d5a0a5967749c8a8b5d0fd856c5fbfe
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2025-01-29 17:25:25 +0000

    Update oidc_auth settings overlay

--- archivematica-apps/archivematica/overlay/src/dashboard/src/settings/components/oidc_auth.artefactual.py
+++ archivematica-apps/archivematica/overlay/src/dashboard/src/settings/components/oidc_auth.artefactual.py
@@ -1,5 +1,7 @@
 import os
 
+from archivematicaFunctions import get_oidc_secondary_providers
+
 OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID", "")
 OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET", "")
 
@@ -7,6 +9,7 @@ OIDC_OP_AUTHORIZATION_ENDPOINT = ""
 OIDC_OP_TOKEN_ENDPOINT = ""
 OIDC_OP_USER_ENDPOINT = ""
 OIDC_OP_JWKS_ENDPOINT = ""
+OIDC_OP_LOGOUT_ENDPOINT = ""
 
 AZURE_TENANT_ID = os.environ.get("AZURE_TENANT_ID", "")
 if AZURE_TENANT_ID:
@@ -23,15 +26,34 @@ if AZURE_TENANT_ID:
         "https://login.microsoftonline.com/%s/discovery/v2.0/keys" % AZURE_TENANT_ID
     )
 else:
-    OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ["OIDC_OP_AUTHORIZATION_ENDPOINT"]
-    OIDC_OP_TOKEN_ENDPOINT = os.environ["OIDC_OP_TOKEN_ENDPOINT"]
-    OIDC_OP_USER_ENDPOINT = os.environ["OIDC_OP_USER_ENDPOINT"]
+    OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get(
+        "OIDC_OP_AUTHORIZATION_ENDPOINT", ""
+    )
+    OIDC_OP_TOKEN_ENDPOINT = os.environ.get("OIDC_OP_TOKEN_ENDPOINT", "")
+    OIDC_OP_USER_ENDPOINT = os.environ.get("OIDC_OP_USER_ENDPOINT", "")
     OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT", "")
+    OIDC_OP_LOGOUT_ENDPOINT = os.environ.get("OIDC_OP_LOGOUT_ENDPOINT", "")
+
+OIDC_SECONDARY_PROVIDER_NAMES = os.environ.get(
+    "OIDC_SECONDARY_PROVIDER_NAMES", ""
+).split(",")
+OIDC_PROVIDER_QUERY_PARAM_NAME = os.environ.get(
+    "OIDC_PROVIDER_QUERY_PARAM_NAME", "secondary"
+)
+OIDC_PROVIDERS = get_oidc_secondary_providers(OIDC_SECONDARY_PROVIDER_NAMES)
+
+if OIDC_OP_LOGOUT_ENDPOINT:
+    OIDC_OP_LOGOUT_URL_METHOD = "components.accounts.views.get_oidc_logout_url"
 
 OIDC_RP_SIGN_ALGO = os.environ.get("OIDC_RP_SIGN_ALGO", "HS256")
 
+
 # Username is email address
-OIDC_USERNAME_ALGO = lambda email: email  # noqa
+def _get_email(email):
+    return email
+
+
+OIDC_USERNAME_ALGO = _get_email
 
 # map attributes from access token
 OIDC_ACCESS_ATTRIBUTE_MAP = {"given_name": "first_name", "family_name": "last_name"}
