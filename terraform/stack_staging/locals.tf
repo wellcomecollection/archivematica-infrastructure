72776f347d7053946de5cd2ddccab0d369a7d229
Santi Collazo <santi@collazo.eu>
2025-07-30 16:47:38 +0200

    Update am images on staging

--- terraform/stack_staging/locals.tf
+++ terraform/stack_staging/locals.tf
@@ -15,10 +15,10 @@ locals {
   }
 
   ecr_image_tags = {
-    dashboard          = "v1.17.1-dc6c3fe896c897e22cded9f69b85742d40227118"
-    mcp_client         = "v1.17.1-dc6c3fe896c897e22cded9f69b85742d40227118"
-    mcp_server         = "v1.17.1-dc6c3fe896c897e22cded9f69b85742d40227118"
-    am_storage_service = "v0.23.0-431eec153c9f97af7d317e5b7bad611cf9614779"
+    dashboard          = "v1.17.1-3f3444807015e98865d9e137ac9d1ae7e26088f0"
+    mcp_client         = "v1.17.1-3f3444807015e98865d9e137ac9d1ae7e26088f0"
+    mcp_server         = "v1.17.1-3f3444807015e98865d9e137ac9d1ae7e26088f0"
+    am_storage_service = "v0.23.0-3f3444807015e98865d9e137ac9d1ae7e26088f0"
     clamavd            = "f60df7b65fe0e405d89191053f7196a1769e4ccf"
     nginx              = "120f7da2bd3a1377974ae1f5523711694d1ba11c"
   }
