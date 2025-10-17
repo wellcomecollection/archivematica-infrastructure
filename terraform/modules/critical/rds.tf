997e94dd59701be01e3ce18969859eb238e93922
Santi Collazo <santi@collazo.eu>
2025-10-17 10:33:32 +0200

    Consolidate rdsupgrade.tf into rds.tf
    There was some duplicated info

--- terraform/modules/critical/rds.tf
+++ terraform/modules/critical/rds.tf
@@ -28,7 +28,7 @@ resource "aws_rds_cluster" "archivematica" {
   vpc_security_group_ids = [aws_security_group.database_sg.id]
 
   engine         = "aurora-mysql"
-  engine_version = "5.7.mysql_aurora.2.11.2"
+  engine_version = "8.0.mysql_aurora.3.08.2"
 }
 
 resource "aws_rds_cluster_instance" "archivematica" {
@@ -65,7 +65,7 @@ resource "aws_rds_cluster_instance" "archivematica" {
 
   apply_immediately = false
 
-  db_parameter_group_name = "default.aurora-mysql5.7"
+  db_parameter_group_name = "default.aurora-mysql8.0"
 }
 
 resource "aws_security_group" "database_sg" {
@@ -101,3 +101,19 @@ resource "aws_security_group" "database_sg" {
 }
 
 // NOTE: When migration is complete, we can remove the aws_rds_cluster, and aws_rds_cluster_instance resources above.
+
+module "aurora_rds_cluster" {
+  source = "../rds"
+
+  cluster_identifier = "${local.cluster_identifier}-mysql8-aurora3"
+  database_name      = local.database_name
+  master_username    = var.rds_username
+  master_password    = var.rds_password
+
+  db_security_group_id     = aws_security_group.database_sg.id
+  aws_db_subnet_group_name = aws_db_subnet_group.archivematica.name
+
+  snapshot_identifier = var.snapshot_identifier
+
+  engine_version = "8.0.mysql_aurora.3.07.1"
+}
