8952821cf3e14c920c968cd5801dc91ac6626a96
Santi Collazo <santi@collazo.eu>
2026-05-07 09:42:52 +0200

    Remove bastion host for staging environment

--- terraform/modules/stack/variables.tf
+++ terraform/modules/stack/variables.tf
@@ -79,8 +79,3 @@ variable "container_host_ami" {
   description = "The AMI to use for the container host"
   type        = string
 }
-
-variable "bastion_host_ami" {
-  description = "The AMI to use for the bastion host"
-  type        = string
-}
