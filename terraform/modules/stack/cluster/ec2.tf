8b58d63d4d9b8c54597f051dcb9c1141fa4e767e
Santi Collazo <santi@collazo.eu>
2026-05-07 09:42:52 +0200

    Remove bastion host for staging environment

--- terraform/modules/stack/cluster/ec2.tf
+++ terraform/modules/stack/cluster/ec2.tf
@@ -4,8 +4,6 @@ module "container_host" {
   cluster_name = var.cluster_name
   vpc_id       = var.vpc_id
 
-  ssh_ingress_security_groups = module.bastion_host.ssh_controlled_ingress_sg
-
   subnets  = var.private_subnets
   key_name = "wellcomedigitalworkflow"
 
@@ -15,19 +13,4 @@ module "container_host" {
 
   ebs_volume_id      = var.ebs_volume_id
   container_host_ami = var.container_host_ami
-}
-
-module "bastion_host" {
-  source = "./bastion_host"
-
-  vpc_id = var.vpc_id
-
-  name = "${var.name}-bastion"
-
-  controlled_access_cidr_ingress = var.controlled_access_cidr_ingress
-
-  key_name         = var.key_name
-  subnet_list      = var.public_subnets
-  bastion_host_ami = var.bastion_host_ami
-
 }
